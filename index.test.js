7d842273f33da6f0b3ee7f786749047192d33b6e
RomanKo <RomanKotsiubynskyi@users.noreply.github.com>
2021-02-27 16:24:13 +0200

    mocha test new

---
+++ index.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const Calc = require ('./calc.js');
+
+
+describe ('Taylor series', function ()
+{
+   t = Calc.Taylor(1);
+   it ('should have a Taylor function defined', function()
+  {
+   assert.equal(typeof Calc.Taylor, "function");
+});
+
+it ('has to be equal to 10', function(){
+    assert.equal(Calc.Taylor(), 9); 
+});
+}
+);
