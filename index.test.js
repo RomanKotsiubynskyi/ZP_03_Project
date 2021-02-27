159a5eacceecf3c443aeb6c46cbf67203841801c
RomanKo <RomanKotsiubynskyi@users.noreply.github.com>
2021-02-27 16:26:37 +0200

    correct 9 to 10

--- index.test.js
+++ index.test.js
@@ -11,7 +11,7 @@ describe ('Taylor series', function ()
 });
 
 it ('has to be equal to 10', function(){
-    assert.equal(Calc.Taylor(), 9); 
+    assert.equal(Calc.Taylor(), 9); // must be 10
 });
 }
 );
